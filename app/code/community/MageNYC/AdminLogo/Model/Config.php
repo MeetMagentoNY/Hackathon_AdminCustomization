089d2adf0d72da3f30faee1b513ae65f5da2c1fc
Manish Jain <manish.jain@aoe.com>
2015-09-20 19:31:38 +0000

    Update config path

--- app/code/community/MageNYC/AdminLogo/Model/Config.php
+++ app/code/community/MageNYC/AdminLogo/Model/Config.php
@@ -3,9 +3,9 @@
 abstract class MageNYC_AdminLogo_Model_Config {
 
   //XML paths for config values
-  const USECUSTOMHEADER = 'design/magenyc_adminlogo/usecustomheader';
-  const CUSTOMLOGO = 'design/magenyc_adminlogo/logo';
-  const ALTTEXT = 'design/magenyc_adminlogo/alt';
-  const LOGOHEIGHT = 'design/magenyc_adminlogo/logo_height';
+  const USECUSTOMHEADER = 'admindesign/admin_header/usecustomheader';
+  const CUSTOMLOGO = 'admindesign/admin_header/logo';
+  const ALTTEXT = 'admindesign/admin_header/alt';
+  const LOGOHEIGHT = 'admindesign/admin_header/logo_height';
 
 }
